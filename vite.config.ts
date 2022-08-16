a472dea46f145122fc12d92590ed55d116a58589
zhjlydia <zhjlydia@foxmail.com>
2022-08-16 22:01:05 +0800

    eslint and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,18 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
-// @ts-ignore
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import viteEslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), reactRefresh()],
+    plugins: [
+        react(),
+        reactRefresh(),
+        viteEslint({
+            failOnError: false,
+        }),
+    ],
     resolve: {
         extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
         alias: {
